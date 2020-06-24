f3d9eab989b2458011cd77402912ef8958464640
dazz2014 <dasuneranda@gmail.com>
2020-06-24 21:54:28 +0530

    add function commit

--- app/src/main/java/com/example/crudsqlite/MyDatabaseHelper.java
+++ app/src/main/java/com/example/crudsqlite/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.crudsqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -61,5 +62,20 @@ class MyDatabaseHelper extends SQLiteOpenHelper {
             }
 
 
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+
+        if (db != null){
+
+        cursor = db.rawQuery(query,null);
+
+        }
+
+        return cursor;
     }
 }
